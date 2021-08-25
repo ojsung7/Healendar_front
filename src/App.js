60a5e159c8bbd54e9f32d156bdd18d660640364a
ojsung7 <ojsung7@gmail.com>
2021-08-25 21:10:57 +0900

    master

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       <Footer />    
     </div>
     </Router>
-    //주석처리
+    //주석처리할거라구요
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
